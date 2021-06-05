a941056eaf63e83a68ca1d6705e2287d668c5bc1
Demir Antay <demir99antay@gmail.com>
2021-06-05 14:49:45 +0300

    started working on the messaging forum

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,5 @@
 # Main Imports
+import random
 
 # Django Imports
 from django.shortcuts import render, get_object_or_404, HttpResponse
@@ -8,12 +9,49 @@ from django.core.files import File
 from django.contrib.auth.models import User
 
 # My Module Imports
-from authentication.models import BasicUserProfile
+from authentication.models import BasicUserProfile, Follower
+from hashtag.models import Topic
+
+from utils.session_utils import get_current_user, get_current_user_profile
+from utils.base_utils import left_nav_tweet_form_processing
+from utils.base_utils import mobile_tweet_form_processing
+from utils.base_utils import get_who_to_follow
+from utils.base_utils import get_topics_to_follow
 
 
 def chat_landing(request):
-    """aa"""
+    """ this is the page that welcomes users to the chat feature """
 
-    data = {}
+    # admin user session pop
+    # admin user session pop
+    # Deleting any sessions regarding top-tier type of users
 
-    return render(request, "chat/landing.html", data)
+    # Get the current users
+    current_basic_user = get_current_user(request, User, ObjectDoesNotExist)
+
+    current_basic_user_profile = get_current_user_profile(
+        request,
+        User,
+        BasicUserProfile,
+        ObjectDoesNotExist
+    )
+
+    # Topics to follow
+    topics_to_follow = get_topics_to_follow(Topic, ObjectDoesNotExist, random)
+
+    # Who to follow box cells
+    who_to_follow = get_who_to_follow(
+        BasicUserProfile, ObjectDoesNotExist, random
+    )
+
+    data = {
+        "current_basic_user": current_basic_user,
+        "current_basic_user_profile": current_basic_user_profile,
+        "who_to_follow": who_to_follow,
+        "topics_to_follow": topics_to_follow,
+    }
+
+    if current_basic_user == None:
+        return HttpResponseRedirect("/auth/signup/")
+    else:
+        return render(request, "chat/landing.html", data)
