afb456cb5e8a033718d02d8c1ce1de3b512f0412
Demir Antay <demir99antay@gmail.com>
2021-06-06 09:18:11 +0300

    shelved chat features for now

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+# Main Imports
 
-# Create your models here.
+# Django Imports
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# My Module Imports
+from authentication.models import BasicUserProfile
+
+
+# Chat
+# ---------
+class Chat(models.Model):
+    creation_date = models.DateField(default=timezone.now)
+    id = models.AutoField(primary_key=True)
+    content = models.TextField()
+    sender = models.ForeignKey(
+        BasicUserProfile,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="sender"
+    )
+    reciever = models.ForeignKey(
+        BasicUserProfile,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="reciever"
+    )
+
+    def __str__(self):
+        return "Topic: " + str(self.sender)
--- chat/views.py
+++ chat/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.models import User
 # My Module Imports
 from authentication.models import BasicUserProfile, Follower
 from hashtag.models import Topic
+from .models import Chat
 
 from utils.session_utils import get_current_user, get_current_user_profile
 from utils.base_utils import left_nav_tweet_form_processing
@@ -91,11 +92,66 @@ def chat_single(request, username):
         BasicUserProfile, ObjectDoesNotExist, random
     )
 
+    # Get the current followings
+    try:
+        current_followings = Follower.objects.filter(
+            follower=current_basic_user_profile
+        )
+    except ObjectDoesNotExist:
+        current_followings = None
+
+    # Get the current text reciever (user)
+    try:
+        current_reciever_user = User.objects.get(username=username)
+    except ObjectDoesNotExist:
+        current_reciever_user = None
+
+    try:
+        current_reciever = BasicUserProfile.objects.get(
+            user=current_reciever_user
+        )
+    except ObjectDoesNotExist:
+        current_reciever = None
+
+    if current_reciever_user == None:
+        return HttpResponseRedirect("/")
+
+    # Getting the chat history feed
+    chat_history_feed = []
+
+    try:
+        chat_records_self = Chat.objects.filter(
+            sender=current_basic_user_profile,
+            reciever=current_reciever
+        )
+        chat_records_reciever = Chat.objects.filter(
+            sender=current_reciever,
+            reciever=current_basic_user_profile,
+        )
+    except ObjectDoesNotExist:
+        chat_records_self = None
+        chat_records_reciever= None
+
+    print(chat_records_self)
+
+    # Chat text from processing
+    if request.POST.get("chat_send_submit_btn"):
+        chat_content = request.POST.get("chat_content")
+        new_message = Chat(
+            content=chat_content,
+            sender=current_basic_user_profile,
+            reciever=current_reciever,
+        )
+        new_message.save()
+        return HttpResponseRedirect("/chat/"+current_reciever_user.username+"/")
+
     data = {
         "current_basic_user": current_basic_user,
         "current_basic_user_profile": current_basic_user_profile,
         "who_to_follow": who_to_follow,
         "topics_to_follow": topics_to_follow,
+        "current_followings": current_followings,
+        "current_reciever": current_reciever,
     }
 
     if current_basic_user == None:
